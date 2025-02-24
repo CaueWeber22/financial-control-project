69c168a79c33937be547f23302a5a8b7e4bb6bea
caue <caueweber22@gmail.com>
2025-02-23 23:59:36 -0300

    Feat: Post and Get, user service and controller

--- financial-control-project/src/main/java/com/fcproject/controllers/UserController.java
+++ financial-control-project/src/main/java/com/fcproject/controllers/UserController.java
@@ -1,9 +1,12 @@
 package com.fcproject.controllers;
 
+import com.fcproject.exception.ResourceNotFound;
+import com.fcproject.models.UserEntity;
 import com.fcproject.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -11,4 +14,24 @@ public class UserController {
 
     @Autowired
     private UserServices userServices;
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody UserEntity user) {
+
+        if (user.getEmail() == null || user.getPassword() == null || user.getFirstName() == null || user.getLastName() == null) {
+
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else {
+
+            return userServices.createUser(user);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<UserEntity> getUserByEmail(@RequestParam String email) {
+        // Calling the user search by email function
+        UserEntity user = userServices.getUserByEmail(email);
+        return new ResponseEntity<>(user, HttpStatus.OK); // Return the user and status 200
+    }
+
 }
--- financial-control-project/src/main/java/com/fcproject/services/UserServices.java
+++ financial-control-project/src/main/java/com/fcproject/services/UserServices.java
@@ -4,7 +4,10 @@ import com.fcproject.exception.ResourceNotFound;
 import com.fcproject.models.UserEntity;
 import com.fcproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
 
 import java.util.logging.Logger;
 
@@ -22,4 +25,21 @@ public class UserServices {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new ResourceNotFound("User Not Found"));
     }
+
+    //User creation
+    public ResponseEntity<String> createUser(UserEntity user) {
+
+        //Checking if the user's email is not null
+        if ((user.getEmail() != null && !user.getEmail().isEmpty())) {
+            userRepository.save(user);
+
+            return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>("User creation failed", HttpStatus.BAD_REQUEST);
+        }
+
+
+
+    }
+
 }
