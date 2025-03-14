7fc976fdd5b0c6b3b6aca7154c8591f9aaa326a0
caue <caueweber22@gmail.com>
2025-03-14 12:00:23 -0300

    Feat: get by email tests

--- financial-control-project/src/test/java/com/fcproject/TestsMocks/UserMocks.java
+++ financial-control-project/src/test/java/com/fcproject/TestsMocks/UserMocks.java
@@ -3,6 +3,7 @@ package com.fcproject.TestsMocks;
 import com.fcproject.data.dto.userDTO.UserDTO;
 import com.fcproject.data.dto.userDTO.UserPostDTO;
 import com.fcproject.data.models.Gender;
+import com.fcproject.data.models.UserEntity;
 
 import java.time.LocalDate;
 
@@ -10,6 +11,20 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class UserMocks {
+
+    public UserEntity mockUserEntity(Integer id) {
+        UserEntity user = new UserEntity();
+
+        user.setId(id);
+        user.setFirstName("Name" + id);
+        user.setLastName("LastName" + id);
+        user.setEmail("test@email.com" + id);
+        user.setPhone("+5511999999999");
+        user.setGender(Gender.MALE);
+        user.setBirthDay(LocalDate.parse("1900-05-05"));
+
+        return user;
+    }
 
     public UserDTO mockUserDTO(Integer id) {
         UserDTO user = new UserDTO();
--- financial-control-project/src/test/java/com/fcproject/services/UserServicesTest.java
+++ financial-control-project/src/test/java/com/fcproject/services/UserServicesTest.java
@@ -2,6 +2,7 @@ package com.fcproject.services;
 
 import com.fcproject.TestsMocks.UserMocks;
 import com.fcproject.data.dto.userDTO.UserDTO;
+import com.fcproject.data.models.Gender;
 import com.fcproject.data.models.UserEntity;
 import com.fcproject.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +15,7 @@ import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,8 +40,21 @@ class UserServicesTest {
     }
 
     @Test
-    void getUserByEmail() {
+    void testGetUserByEmail() {
+        Integer id = 1;
+        UserEntity userEntityMock = input.mockUserEntity(id); // User mocked for test
+        when(userRepository.findByEmail("test@email.com" + id)).thenReturn(Optional.of(userEntityMock));
 
+        UserDTO result = userServices.getUserByEmail("test@email.com" +id);
+
+        //Asserts
+        assertNotNull(result);
+        assertEquals("test@email.com"+id, result.getEmail());
+        assertEquals("LastName"+id, result.getLastName());
+        assertEquals("Name"+id, result.getFirstName());
+        assertEquals("+5511999999999", result.getPhone());
+        assertEquals(LocalDate.parse("1900-05-05"), result.getBirthDay());
+        assertEquals(Gender.MALE, result.getGender());
     }
 
     @Test
